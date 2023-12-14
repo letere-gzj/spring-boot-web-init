d5fc9534f8c57fb51e3a5670abba903d1daf0e5e
gaozijie <1906497996@qq.com>
2023-12-14 17:43:28 +0800

    补充跨域配置

---
+++ src/main/java/com/letere/demo/common/config/CorsConfig.java
@@ -0,0 +1,34 @@
+package com.letere.demo.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/**
+ * 跨域配置
+ * @author gaozijie
+ * @since 2023-12-14
+ */
+@Configuration
+public class CorsConfig {
+
+    private CorsConfiguration getConfig() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        corsConfiguration.setMaxAge(3600L);
+        corsConfiguration.setAllowCredentials(false);
+        return corsConfiguration;
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", this.getConfig());
+        return new CorsFilter(source);
+    }
+}
+
